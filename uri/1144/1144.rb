f0fdafd9b7ba930eff96da4d954843c74caf2b26
tkovs <victor.rdg@hotmail.com>
2016-04-17 22:26:16 -0300

    URI, Ruby, 1144

---
+++ uri/1144/1144.rb
@@ -0,0 +1,18 @@
+bb = 2
+a = 1
+b = 1
+c = 1
+
+n = gets.chomp.to_i
+
+n.times do
+    puts "#{a} #{b} #{c} "
+    b += 1
+    c += 1
+    puts "#{a} #{b} #{c} "
+    a += 1
+
+    b += bb
+    bb += 2
+    c = a * b
+end
