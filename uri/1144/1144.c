ab9c12d0b156ecd0e14c44d9fc35764211b22b1e
tkovs <victor.rdg@hotmail.com>
2016-04-18 19:54:31 -0300

    URI, C, 1144

---
+++ uri/1144/1144.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int bb = 2;
+    int a = 1, b = 1, c = 1;
+    int n, i;
+    
+    scanf ("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf ("%d %d %d\n", a, b++, c++);
+        printf ("%d %d %d\n", a++, b, c);
+        
+        b += bb;
+        bb += 2;
+        c = a * b;
+    }
+
+    return 0;
+}
+
